9ce914251446e9e33ebb00c9f3624534e2e88722
Muh Ridha Ramadhan Fatriendi <ramadhan13@student.untan.ac.id>
2021-06-24 15:57:42 +0700

    UAS PBO-2021

---
+++ application/Models/Dosen.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+require_once('Pengguna.php');
+
+class Dosen extends Pengguna implements Pegawai
+{
+    private $nip;
+    private $nama;
+    private $no_telp;
+
+    public function validasiMahasiswa()
+    {
+    }
+
+    public function setNip($nip)
+    {
+        $this->nip = $nip;
+    }
+
+
+    public function presensiMasuk()
+    {
+    }
+
+    public function getNip()
+    {
+        return $this->nip;
+    }
+
+    public function setNama($nama)
+    {
+        $this->nama = $nama;
+    }
+
+    public function getNama()
+    {
+        return $this->nama;
+    }
+}
